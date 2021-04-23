4cd581e57b86ab3d2da747cb72f7ec8d6043ae6b
Cody Brunner <rockchalkwushock@icloud.com>
2021-04-22 19:51:02 -0500

    test(src): adds tests for plugin, updates jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   bail: true,
-  collectCoverage: true,
   collectCoverageFrom: ['src/index.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
   preset: 'ts-jest',
   roots: ['<rootDir>'],
   testEnvironment: 'node',
+  transform: { '^.+\\.(ts|tsx|js|jsx)?$': 'ts-jest' },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
 }
---
+++ src/index.test.ts
@@ -0,0 +1,45 @@
+import rehype from 'rehype'
+import dedent from 'dedent'
+import rehypeCodeTitles from './index'
+
+const processHtml = (html: string): string => {
+  return rehype()
+    .data('settings', { fragment: true })
+    .use(rehypeCodeTitles)
+    .processSync(html)
+    .toString()
+}
+
+const classNameWithTitle = 'language-typescript:lib/mdx.ts'
+const className = 'language-typescript'
+
+describe('rehype-code-titles', () => {
+  test('Snapshot w/ title block', () => {
+    const result = processHtml(dedent`
+      <pre><code class="${classNameWithTitle}"></code></pre>
+    `)
+    expect(result).toMatchSnapshot()
+  })
+  test('Snapshot w/out title block', () => {
+    const result = processHtml(dedent`
+      <pre><code class="${className}"></code></pre>
+    `)
+    expect(result).toMatchSnapshot()
+  })
+  test('Contains title block', () => {
+    const result = processHtml(dedent`
+      <pre><code class="${classNameWithTitle}"></code></pre>
+    `)
+    expect(result.includes('div')).toEqual(true)
+    expect(result.includes('rehype-code-title')).toEqual(true)
+    expect(result.includes('lib/mdx.ts')).toEqual(true)
+  })
+  test('Does not contain title block', () => {
+    const result = processHtml(dedent`
+      <pre><code class="${className}"></code></pre>
+    `)
+    expect(result.includes('div')).not.toEqual(true)
+    expect(result.includes('rehype-code-title')).not.toEqual(true)
+    expect(result.includes('lib/mdx.ts')).not.toEqual(true)
+  })
+})
