f67e39e02f170644b082e8c19a61515d6f60edf5
Taranveer Bains <taranveer786@gmailcom>
2023-02-14 11:41:23 -0800

    Chore: update core dependencies to satisfy typescript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { BuildVisitor, visit } from 'unist-util-visit'
+import { visit } from 'unist-util-visit'
 import type * as H from 'hast'
-import type { VisitorResult } from 'unist-util-visit'
+import type { BuildVisitor, VisitorResult } from 'unist-util-visit'
 
 type Options = {
   customClassName?: string
